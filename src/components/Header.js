2bc7c69cf6a545d4759f69201bd5febc22ca4bfd
Emir AL <mythia@windowslive.com>
2021-12-23 12:33:08 +0300

    feat: add position fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <header className='bg-zinc-100 dark:bg-zinc-900 h-12 shadow-md dark:shadow-black/30 flex items-center justify-between px-4'>
+    <header className='bg-zinc-100 dark:bg-zinc-900 h-12 shadow-md dark:shadow-black/30 flex items-center justify-between px-4 fixed left-0 top-0 w-screen'>
       <h5 className='text-lg'>Santa Dashboard</h5>
       <button className='h-10 w-10' onClick={() => setMenuOpen(!menuOpen)}>
         {menuOpen ? (
