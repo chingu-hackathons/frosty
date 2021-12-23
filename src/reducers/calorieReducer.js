53401e994c521b160653a29304635682b05c2013
Emir AL <mythia@windowslive.com>
2021-12-23 15:06:50 +0300

    feat: add home target & start/stop toggle cases

--- src/reducers/calorieReducer.js
+++ src/reducers/calorieReducer.js
@@ -1,5 +1,18 @@
 export const calorieReducer = (state, action) => {
   switch (action.type) {
+    case 'SET_HOME_TARGET':
+      return {
+        ...state,
+        homes: {
+          ...state.homes,
+          target: action.target,
+        },
+      };
+    case 'TOGGLE_START_STOP':
+      return {
+        ...state,
+        start: !state.start,
+      };
     default:
       return state;
   }
@@ -10,9 +23,9 @@ export const defaultState = {
   milk: 0,
   tea: 0,
   homes: {
+    target: 1000,
     visited: 0,
-    remaining: 0,
   },
-  log: [5000],
+  log: [5000, 2445, 4555, 4352, 4535, 4564, 6575],
   start: false,
 };
