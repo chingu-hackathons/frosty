702b718593b8d82116cc19130bf93cf4920e83b6
Emir AL <mythia@windowslive.com>
2021-12-24 13:04:15 +0300

    feat: fix styling of input

--- src/components/Input.js
+++ src/components/Input.js
@@ -15,10 +15,12 @@ const Input = () => {
   };
 
   return (
-    <div>
-      <label htmlFor='input'>Homes to visit:</label>
+    <div className='mb-4'>
+      <label htmlFor='input' className='mr-2'>
+        Homes to visit:
+      </label>
       <input
-        className='rounded-full text-center px-2 w-20'
+        className='rounded-full text-center px-2 w-20 bg-zinc-100 dark:bg-zinc-900'
         type='number'
         min={1}
         id='input'
